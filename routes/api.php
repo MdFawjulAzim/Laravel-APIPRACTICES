12be4aba749819f83ce7ec2230b81d45a94c22bc
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2025-06-12 18:33:06 +0600

    Get Method

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/test', function () {
+    return ['name' => "anilsidhu", 'channel' => "Code by step by step"];
+});
+
+
+Route::get("student", [StudentController::class, "list"])->name("student");
